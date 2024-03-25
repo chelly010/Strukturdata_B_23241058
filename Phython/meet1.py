cab4b45131e1372992dec15010aaf13b89d210aa
chelly010 <146046103+chelly010@users.noreply.github.com>
2024-03-25 12:05:50 +0800

    senin_25/03/2024

---
+++ Phython/meet1.py
@@ -0,0 +1,7 @@
+#perkenalan diri MK.Struktur data
+
+print("Nama saya chelly satya")
+print("NIM 23241058")
+print("Semester II/B")
+print("Saya mahasiswa Undikma")
+print("Saya mengambil prodi PTI")
