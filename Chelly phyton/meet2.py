bffbf57ace2c6b396df9caff08431b37d426a107
chelly010 <146046103+chelly010@users.noreply.github.com>
2024-04-01 12:04:10 +0800

    senin/01/04/2024

---
+++ Chelly phyton/meet2.py
@@ -0,0 +1,18 @@
+# Belajar Variable
+# Assigment
+x = 3
+y = 6
+uang = 1000
+
+# Pemanggilan
+print ("Nilai UAS saya =", x)
+print ("Nilai UTS saya =", y)
+print ("Sisa Uang saya =", uang)
+
+# Penamaan Variable
+nilai_a = 95 #Tidak boleh spasi
+kotak10 = 10 #Tidak boleh diawali angka
+
+x = 30
+ #input variable 
+print ("Nilai UAS saya =", x)
