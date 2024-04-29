b849a1984cd0085116b69b420aee2a2e5dadbb4e
chelly010 <146046103+chelly010@users.noreply.github.com>
2024-04-29 11:56:14 +0800

    senin 29/4/2024

---
+++ Chelly phyton/meet4&5.py
@@ -0,0 +1,70 @@
+# inputan dari user
+# aritmatika 
+
+# belajar inputan
+chelly = input("masukan kata : ")
+print("isi dari chelly : ", chelly,"bertipe data :", type(chelly)),
+
+
+# belajar arimatika dasar
+
+# contoh dri penjumlahan +
+x = 3
+y = 4
+
+hasil = x + y
+print (" x + y =", hasil)
+
+# contoh dri pengurangan -
+x = 3
+y = 4
+
+hasil = x - y
+print (" x - y =", hasil)
+
+# contoh dri perkalian *
+x = 3
+y = 4
+
+hasil = x * y
+print (" x * y =", hasil)
+
+# contoh dri pembagian /
+x = 3
+y = 4
+
+hasil = x / y
+print (" x / y =", hasil)
+
+# perpangkatan exponen **
+x = 3
+y = 4
+
+hasil = x ** y
+print (" x ^ y =", hasil)
+
+# modulus %
+x = 3
+y = 4
+
+hasil = y % x
+print (" x % y =", hasil)
+
+# floor division (pembulatan kebawah) //
+x = 11
+y = 4
+
+hasil = x // y
+print (" x // y =", hasil)
+
+# aritmatika prioritas
+
+# () , exponen , perkalian atau pembagian , penjumlahan atau pengurangan
+
+# contoh
+a = 3
+b = 4 
+c = 5
+
+hasil = ( a ** b * (c + a) / b - c % a // b)
+print(hasil)
