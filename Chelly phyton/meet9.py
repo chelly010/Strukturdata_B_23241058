1663fef3586431f8cb754f89db40741e365eee45
chelly010 <146046103+chelly010@users.noreply.github.com>
2024-06-03 11:50:30 +0800

    Senin 3/6/2024

---
+++ Chelly phyton/meet9.py
@@ -0,0 +1,27 @@
+# Percabangan jika maka
+
+nama = input("What is Your Name : ")
+
+
+#If kondisi : Aksi 
+
+#If Standar
+
+if nama == "edy" : print("Dosen ganteng")
+print("end of program")
+
+#If Indentasi
+
+if nama == "edy" :
+    print("Dosen ganteng")
+    print("Ganteng Banget")
+print("end of program")
+
+# IF Else
+
+if nama == "edy" :
+    print("Dosen ganteng")
+    print(f"Terima kasih {nama}") 
+else :
+    print("Anda Tidak Ganteng")
+print("end of program")
